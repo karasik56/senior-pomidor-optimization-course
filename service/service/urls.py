7400c06122f6342eb00800dda613ca6c18c86188
Vladimir Karimov <shutpunk@gmail.com>
2023-01-04 15:20:57 +0500

    Day 3 - 1

--- service/service/urls.py
+++ service/service/urls.py
@@ -16,6 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from rest_framework.routers import DefaultRouter
+
+from services.views import SubscriptionView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
 ]
+
+router = DefaultRouter()
+router.register(r'api/subscriptions', SubscriptionView)
+
+urlpatterns += router.urls
--- service/services/views.py
+++ service/services/views.py
@@ -1,3 +1,14 @@
+from django.db.models import Prefetch
 from django.shortcuts import render
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
-# Create your views here.
+from clients.models import Client
+from services.models import Subscription
+from services.serializers import SubscriptionSerializer
+
+
+class SubscriptionView(ReadOnlyModelViewSet):
+    queryset = Subscription.objects.all().prefetch_related(
+        Prefetch('client', queryset=Client.objects.all().select_related('user').only('company_name', 'user__email'))
+    )
+    serializer_class = SubscriptionSerializer
