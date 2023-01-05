0f10edea86f154f55e983bbd578f644ee7bd26a0
Vladimir Karimov <shutpunk@gmail.com>
2023-01-05 16:19:22 +0500

    Day 3 - 2

--- service/clients/models.py
+++ service/clients/models.py
@@ -6,3 +6,6 @@ class Client(models.Model):
     user = models.OneToOneField(User, on_delete=models.PROTECT)
     company_name = models.CharField(max_length=100)
     full_address = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.user}"
--- service/services/models.py
+++ service/services/models.py
@@ -8,6 +8,9 @@ class Service(models.Model):
     name = models.CharField(max_length=50)
     full_price = models.PositiveIntegerField()
 
+    def __str__(self):
+        return f"{self.name}"
+
 
 class Plan(models.Model):
     PLAN_TYPES = (
@@ -18,8 +21,14 @@ class Plan(models.Model):
     plan_type = models.CharField(choices=PLAN_TYPES, max_length=10)
     discount_percent = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(100)])
 
+    def __str__(self):
+        return f"{self.plan_type}"
+
 
 class Subscription(models.Model):
     client = models.ForeignKey(Client, related_name='subscriptions', on_delete=models.PROTECT)
     service = models.ForeignKey(Service, related_name='subscriptions', on_delete=models.PROTECT)
     plan = models.ForeignKey(Plan, related_name='subscriptions', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f"Подписка на {self.service} пользователем {self.client} с тарифом {self.plan}"
